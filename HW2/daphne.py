994a32acf5dd093242ea3634c40b482d71811b92
Emma Hansen <emmah0@uw.edu>
2021-02-08 12:49:58 -0800

    committing because I finally have the right error

--- HW2/daphne.py
+++ HW2/daphne.py
@@ -1,12 +1,12 @@
 import json
 import subprocess
 
-def daphne(args, cwd='../daphne'):
+def daphne(args, cwd='../daphne/'):
     """daphne calling function """
     #proc = subprocess.run(['lein','run','-f','json'] + args,
     #                      capture_output=True, cwd=cwd)
     proc = subprocess.run(['lein','run','-f','json'] + args,
-                          stdout=subprocess.PIPE,stderr=subprocess.PIPE, cwd=cwd)
+                          stdout=subprocess.PIPE,stderr=subprocess.PIPE, cwd=cwd, shell=True)
     if(proc.returncode != 0):
         raise Exception(proc.stderr.decode())
     return json.loads(proc.stdout)
