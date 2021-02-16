a0a09c2492b94bbfed3dcf8875850e1886e671e2
Emma Hansen <emmah0@uw.edu>
2021-02-15 18:28:54 -0800

    4.daphne case runs w/o error, but now 1.daphne errors out

--- HW2/graph_based_sampling.py
+++ HW2/graph_based_sampling.py
@@ -15,6 +15,7 @@ env = {'normal': dist.Normal,
        '+': torch.add,
        '-': torch.sub,
        '/': torch.div,
+       '*': torch.mul,
        'vector': vector,
        'put': put,
        'sample*': sampleS,
@@ -26,7 +27,14 @@ env = {'normal': dist.Normal,
        'if': primitif,
        '<': leq,
        '>': geq,
-       'let': nested_search}
+       'let': nested_search,
+       'observe*': observeS,
+       'discrete': dist.Categorical,
+       'mat-transpose': transpose,
+       'mat-tanh': torch.tanh,
+       'mat-add': torch.add,
+       'mat-mul': matmul,
+       'mat-repmat': repmat}
 
 
 def deterministic_eval(exp):
@@ -40,7 +48,7 @@ def deterministic_eval(exp):
         # print('return value is: ',str(env[op](*map(deterministic_eval, args))))
         if op=='hash-map':
             args =  [args[i][j] for i in range(np.shape(args)[0]) for j in range(np.shape(args)[1])]
-        print('type of args is: ',str(type(args)))
+        # print('type of args is: ',str(type(args)))
 
         return env[op](*map(deterministic_eval, args))
     elif type(exp) is int or type(exp) is float:
@@ -52,42 +60,69 @@ def deterministic_eval(exp):
 
 def sample_from_joint(graph):
     "This function does ancestral sampling starting from the prior."
-    # TODO insert your code here
+    # Need to order random vars based on parent-child structure given in A
     V = graph[1]['V']
     A = graph[1]['A']
     P = graph[1]['P']
     Y = graph[1]['Y']
 
-    out_key = graph[2]
+    V_parent = []
+    children = list(set([list(A.values())[i][j] for i in range(len(list(A.values()))) for j in range(len(list(A.values())[i]))]))
+    # print('V is: ',str(V))
+    # print('parent-child relationships: ',str(A))
+    for var in V:
+        if var in list(A.keys()):
+            if var not in children:
+                V_parent = V_parent + [var]
+            
+    V_ordered = []
+    ordered = sort_variables([],V_parent,A)
+    for i in range(len(ordered)):
+        if ordered[i] not in V_ordered:
+            V_ordered = V_ordered + [ordered[i]]      
     
     # print(V)
     # print(P)
     var_samples = {}
-    for var in V:
+    for var in V_ordered:
         if 'current_keys' in locals():
-            print('current_keys exists')
-            print(var,' probability is: ',str(P[var]))
+            # print('current_keys exists')
+            # print(var,' probability is: ',str(P[var]))
             new_P = P[var]
             for key_name in current_keys:
-                print('Updating with prev. random var values')
+                # print('Updating with prev. random var values')
                 # new_P = ['let', key_name, var_samples[key_name], new_P]
                 new_P = nested_search(key_name,float(var_samples[key_name]), new_P)
-                print('new statement is: ',str(new_P))
+                # print('new statement is: ',str(new_P))
 
             var_samples[var] = deterministic_eval(new_P)
-            print(var,' value is: ',str(var_samples[var]))
+            # print(var,' value is: ',str(var_samples[var]))
             
             current_keys = list(var_samples.keys())
         else:
-            print(var,' probability is: ',str(P[var]))
+            # print(var,' probability is: ',str(P[var]))
             var_samples[var] = deterministic_eval(P[var])
-            print(var,' value is: ',str(var_samples[var]))
+            # print(var,' value is: ',str(var_samples[var]))
 
             current_keys = list(var_samples.keys())
         
-            
+    # print('Samples are: ',str(var_samples))
 
-    return var_samples[out_key]
+    out_key = graph[2]
+    for key_name in current_keys:
+        out_key = nested_search(key_name,float(var_samples[key_name]), out_key)
+    return deterministic_eval(out_key)
+    
+    # if graph[2][0] == 'vector':
+    #     out_key = graph[2][1:]
+    #     print('outkey is: ',str(out_key))
+    #     return [var_samples[key] for key in out_key]
+    # else:
+    #     out_key = graph[2]
+    #     print('outkey is: ',str(out_key))
+    #     return var_samples[out_key]
+    
+    
 
 
 def get_stream(graph):
@@ -131,7 +166,7 @@ def run_probabilistic_tests():
     num_samples=1
     max_p_value = 1e-4
     
-    for i in range(5,7):
+    for i in range(1,7):
         #note: this path should be with respect to the daphne path!   
         if i != 4:
 
@@ -156,14 +191,15 @@ if __name__ == '__main__':
     
 
     # run_deterministic_tests()
-    run_probabilistic_tests()
+    # run_probabilistic_tests()
 
 
 
 
-    # for i in range(1,5):
-    #     graph = daphne(['graph','-i','../CS532-HW2/programs/{}.daphne'.format(i)])
-    #     print('\n\n\nSample of prior of program {}:'.format(i))
-    #     print(sample_from_joint(graph))    
+    for i in range(1,5):
+        graph = daphne(['graph','-i','../HW2/programs/{}.daphne'.format(i)])
+        print('graph is:',str(graph))
+        print('\n\n\nSample of prior of program {}:'.format(i))
+        print(sample_from_joint(graph))    
 
     
--- HW2/primitives.py
+++ HW2/primitives.py
@@ -4,19 +4,39 @@ import torch
 
 def vector(*vals):
     print('vector is called')
-    output = [vals[i] for i in range(len(vals))]
-    return torch.Tensor(output)
+    # print('values are: ',str(vals))
+    if issubclass(type(vals[0]),torch.distributions.distribution.Distribution):
+        output = [vals[i] for i in range(len(vals))]
+        return output
+    else:
+        try:
+            length = len(vals[0])
+            # print('length is ',str(length))
+        except:
+            # print('length is 1')
+            length = 1
+
+        if length > 1:
+            # output = [val[i] for val in vals for i in range(len(val))]
+            # print('vector is creating a matrix')
+            output = torch.cat(vals,1)
+            return output
+        else:
+            output = [vals[i] for i in range(len(vals))]
+            return torch.Tensor(output).resize_((len(output),1))
+        # print(type(output[0]))
+        
 
 def put(*vals):
     print('put is called')
     vec = vals[0]
-    print('vec type is: ',str(type(vec)))
+    # print('vec type is: ',str(type(vec)))
     if isinstance(vec,dict):
         index = float(vals[1])
     else:
         index = int(vals[1])
     value = vals[2]
-    print('value type is: ',str(type(value)))
+    # print('value type is: ',str(type(value)))
 
     vec[index] = value
     return vec
@@ -28,7 +48,7 @@ def sampleS(dist):
 
 def hashmap(*vals):
     print('hashmap is called')
-    print('input is: ',str(vals))
+    # print('input is: ',str(vals))
     # keys = [vals[i][0] for i in range(len(vals))]
     # values = [vals[i][1:] for i in range(len(vals))]
     keys = [vals[i] for i in range(0,len(vals),2)]
@@ -41,10 +61,12 @@ def get(*vals):
     print('get is called')
     obj = vals[0]
     index = vals[1]
-    return obj[index]
-    # if isinstance(obj,dict):
-    #     return obj[vals]
-    # else:
+    
+    if isinstance(obj,dict):
+        return obj[index]
+    else:
+        # print('index is: ',str(int(index)))
+        return obj[int(index)]
 
 def append(*vals):
     vec = vals[0]
@@ -54,7 +76,7 @@ def append(*vals):
 def let(vals): # not used
     print('let is called')
     v = vals[0] # will be a string?
-    print('binding name is: ',v)
+    # print('binding name is: ',v)
     e1 = vals[1] # will be a value?
     e2 = vals[2] # will be a string representing an expression?
 
@@ -68,7 +90,7 @@ def primitif(*vals):
     true_value = vals[1]
     false_value = vals[2]
 
-    print('logical type:',str(type(logical)))
+    # print('logical type:',str(type(logical)))
 
     if logical == True:
         return true_value
@@ -88,18 +110,62 @@ def geq(*vals):
         return False
 
 def nested_search(key,val,exp): # this is my let function actually
+    print('let/nested search is called')
     length = len(exp)
     if type(exp) is list:
         for i in range(length):
             if type(exp[i]) is list:
-                print('type is list')
+                # print('type is list')
                 nested_search(key,val,exp[i])
             else: 
-                print('checking for key')
-                print(exp[i])
+                # print('checking for key')
+                # print(exp[i])
                 if exp[i] == key:
-                    print('key found')
+                    # print('key found')
                     exp[i] = val
     return exp
 
-            
+def observeS(*vals):
+    distribution = vals[0]
+    rand_var = vals[1]
+
+    return torch.exp(distribution.log_prob(rand_var))
+
+def sort_variables(V_ordered,V_parent,A):
+    V_ordered = [] + V_parent
+    for key in V_parent:
+        if key in list(A.keys()):
+            V_child = A[key]
+            if V_child not in V_ordered:
+                V_ordered = V_ordered + V_child
+                V_ordered = V_ordered + sort_variables(V_ordered,V_child,A)
+    return V_ordered
+
+def transpose(*vals):
+    print('transpose is called')
+    # print('vals are: ',str(vals))
+    if len(vals) == 3 and isinstance(vals[1],int):
+        return torch.transpose(vals)
+    else:
+        # vals = vals.resize_((list(vals.shape)[0],1))
+        return torch.transpose(vals[0],0,1)
+
+def repmat(*vals):
+    print('repmat is called')
+    # print('input is: ',str(vals))
+    tensor = vals[0]
+    reps = tuple([int(val) for val in vals[1:]])
+    return tensor.repeat(reps)
+
+def matmul(*vals):
+    print('matmul is called')
+    size1 = list(vals[0].shape)
+    size2 = list(vals[1].shape)
+
+    if size1[1] != size2[0]:
+        if size1[0] == size2[0]:
+            return torch.matmul(torch.transpose(vals[0],0,1),vals[1])
+        else:
+            return torch.matmul(vals[0],torch.transpose(vals[1],0,1))
+    else:
+        return torch.matmul(vals[0],vals[1])
